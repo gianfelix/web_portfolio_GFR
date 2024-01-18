8ef0c771daae2884ac3bc270a2c67d19c8b54e4b
Gian Felix Ramadan <gfelixramadan@gmail.com>
2024-01-18 11:25:56 +0700

    update webgis2

--- src/components/WebGIS/WebGIS2.jsx
+++ src/components/WebGIS/WebGIS2.jsx
@@ -1,22 +1,17 @@
 import React from 'react';
-import { MapContainer, TileLayer, WMSTileLayer, Popup } from 'react-leaflet';
 
 const WebGIS2 = () => {
   return (
-    <MapContainer center={[-3, 117]} zoom={6} style={{ zIndex: 0, height: '93vh', width: '100%' }}>
-      <TileLayer
-        url="http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      <WMSTileLayer
-        layers="Test_shp:INDONESIA_KAB"
-        url="http://localhost:8080/geoserver/Test_shp/wms?"
-        transparent={true}
-        format="image/png"
-        attribution="&copy; <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a>"
-      >
-        
-      </WMSTileLayer>
-    </MapContainer>
+    <div>
+      
+      <iframe
+        title="Indonesia Kabupaten Map"
+        src="https://indonesia-kab-map.netlify.app/"
+        width="100%"
+        height="900px"
+        frameBorder="0"
+      ></iframe>
+    </div>
   );
 };
 
